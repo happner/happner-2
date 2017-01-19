cec1ea086d65c5ea9fcc7fc91bb60a8ea8a69229
Simon Bishop <simonbishop@Simons-MacBook-Pro.local>
2017-01-19 18:46:38 +0200

    have test failing

--- test/b4-permissions-translation.js
+++ test/b4-permissions-translation.js
@@ -49,6 +49,8 @@ var should = require('chai').should();
 var mesh;
 var Mesh = require('../');
 
+var dbFileName = './temp/permissions.nedb';
+
 var adminClient = new Mesh.MeshClient({secure: true});
 var test_id = Date.now() + '_' + require('shortid').generate();
 var async = require('async');
@@ -69,6 +71,8 @@ describe('b4 - component start and validation -', function () {
     mesh.initialize({
       name: 'b4_permissions_translation',
       happn: {
+        persist: true,
+        filename: dbFileName,
         secure: true,
         adminPassword: test_id,
       },
