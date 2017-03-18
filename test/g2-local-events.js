c0161b7c81231b74bf87db4d66cd48f71642ad32
nomilous <nomilous@gmail.com>
2017-03-18 14:03:47 +0200

    local events

---
+++ test/g2-local-events.js
@@ -0,0 +1,72 @@
+var expect = require('expect.js');
+var Happner = require('..');
+
+describe('g2 - local events', function () {
+
+  var server;
+
+  before('start server', function (done) {
+    Happner.create({
+      modules: {
+        'component1': {
+          instance: {
+            start: function ($happn, callback) {
+              setInterval(function () {
+
+
+                $happn.localEmit('eventName', {some: 'data'});
+
+
+              }, 100);
+              callback();
+            }
+          }
+        },
+        'component2': {
+          instance: {
+            start: function ($happn, callback) {
+              callback();
+            },
+            awaitLocalEvent: function ($happn, callback) {
+
+
+              $happn.localEvent.component1.once('eventName', function (data) {
+                callback(null, data);
+              });
+
+
+            }
+          }
+        }
+      },
+      components: {
+        'component1': {
+          startMethod: 'start'
+        },
+        'component2': {
+          startMethod: 'start'
+        }
+      }
+    })
+      .then(function (_server) {
+        server = _server;
+      })
+      .then(done).catch(done);
+  });
+
+  after('stop server', function (done) {
+    if (!server) return done();
+    server.stop({reconnect: false}, done);
+  });
+
+  it('can subscribe to and emit local events', function (done) {
+
+    server.exchange.component2.awaitLocalEvent()
+      .then(function (result) {
+        expect(result).to.eql({some: 'data'});
+      })
+      .then(done).catch(done);
+
+  });
+
+});
