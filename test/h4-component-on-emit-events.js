da12e96a2f6fde4f74ee9f07f98d49a5bd3065b7
southbite <simon@tenacious.digital>
2017-09-27 14:54:07 +0200

    enhance: adding onEmitError and OK events

---
+++ test/h4-component-on-emit-events.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var filename = path.basename(__filename);
+
+describe(filename, function () {
+
+  before('start a mesh with 2 components, and a client', function (done) {
+
+  });
+
+  after('stop the mesh', function (done) {
+
+  });
+
+  it('tests an emit using the default transactional consistency, checks the on-emit-ok event', function (done) {
+
+  });
+
+  it('tests an emit using the queued transactional consistency, checks the on-emit-ok event', function (done) {
+
+  });
+
+  it('tests an emit using the acknowledged transactional consistency, checks the on-emit-ok event', function (done) {
+
+  });
+
+  it('tests an emit using the acknowledged transactional consistency, checks the on-emit-ok event', function (done) {
+
+  });
+
+  it('tests a failing event emit makes it to the on-emit-error event', function (done) {
+
+  });
+
+
+});
