42b2bd086d12ccd0c59b9365342c6c8ff6a48f0d
southbite <simon@tenacious.digital>
2019-03-06 17:10:17 +0200

    test: temporarily running db-compaction only for travis failure

--- test/integration/data/db-compaction.js
+++ test/integration/data/db-compaction.js
@@ -4,13 +4,20 @@ const { exec } = require('child_process');
 describe.only(require('../../__fixtures/utils/test_helper').create().testName(__filename, 3), function () {
 
   it('test to see that the child process does exit if database filename and compactInterval params have been specified', function (done) {
-    this.timeout(5000);
+
+    this.timeout(10000);
 
     var procPath = require('path').resolve(__dirname, '../../__fixtures/test/integration/data/db-compaction.js');
 
     const ls = exec('node ' + procPath);
 
     ls.stderr.on('data', (data) => {
+      console.log('stderr:::', data);
+      done(data);
+    });
+
+    ls.stdout.on('data', (data) => {
+      console.log('stdout:::', data);
       done(data);
     });
 
