cbbba3aa4cc47b0e003b69241d846d7ed75ea3e8
southbite <simon@tenacious.digital>
2019-03-06 10:05:25 +0200

    test: removed db compaction test

--- test/integration/data/db-compaction.js
+++ test/integration/data/db-compaction.js
@@ -3,7 +3,7 @@ const { exec } = require('child_process');
 
 describe(require('../../__fixtures/utils/test_helper').create().testName(__filename, 3), function () {
 
-  it('test to see that the child process does exit if database filename and compactInterval params have been specified', function (done) {
+  xit('test to see that the child process does exit if database filename and compactInterval params have been specified', function (done) {
     this.timeout(5000);
 
     var procPath = require('path').resolve(__dirname, '../../__fixtures/test/integration/data/db-compaction.js');
