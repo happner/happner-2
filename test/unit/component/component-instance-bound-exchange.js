6e228e43d6d28a495b615cbd3d139aa0716e0b67
southbite <simon@tenacious.digital>
2019-05-20 17:47:15 +0200

    test: bound exchange caching

---
+++ test/unit/component/component-instance-bound-exchange.js
@@ -0,0 +1,96 @@
+describe(require('../../__fixtures/utils/test_helper').create().testName(__filename, 3), function () {
+
+  var expect = require('expect.js');
+
+  it('tests the clearCachedBoundExchange function, uninitalized cache', function(){
+    var ComponentInstance = require('../../../lib/system/component-instance');
+    var componentInstance = new ComponentInstance();
+    componentInstance.clearCachedBoundExchange();
+  });
+
+  it('tests the clearCachedBoundExchange function, initialized cache', function(done){
+    var ComponentInstance = require('../../../lib/system/component-instance');
+    var componentInstance = new ComponentInstance();
+    componentInstance.boundExchangeCache = {
+      clear:done
+    }
+    componentInstance.clearCachedBoundExchange();
+  });
+
+  it('tests the initializeCachedBoundExchange function, initialized cache', function(done){
+    var ComponentInstance = require('../../../lib/system/component-instance');
+    var componentInstance = new ComponentInstance();
+    var EventEmitter = require('events').EventEmitter;
+    var eventEmitter = new EventEmitter();
+    var mesh = {
+      config:{
+        boundExchangeCacheSize:5
+      },
+      happn:{
+        server:{
+          services:{
+            security:eventEmitter,
+            cache:{
+              new:function(key, opts){
+                expect(opts).to.eql({
+                  type: 'LRU',
+                  cache: {
+                    max: mesh.config.boundExchangeCacheSize || 10000
+                  }
+                });
+                return {
+                  clear:done
+                };
+              }
+            }
+          }
+        }
+      }
+    }
+
+    componentInstance.initializeCachedBoundExchange(mesh, 'test-component');
+    mesh.happn.server.services.security.emit('security-data-changed', {});
+  });
+
+  it('tests the getCachedBoundExchange function, uninitialized cache', function(){
+    var ComponentInstance = require('../../../lib/system/component-instance');
+    var componentInstance = new ComponentInstance();
+    expect(componentInstance.getCachedBoundExchange({
+      username:'test-user'
+    })).to.be(null);
+  });
+
+  it('tests the getCachedBoundExchange function, initialized cache', function(){
+    var ComponentInstance = require('../../../lib/system/component-instance');
+    var componentInstance = new ComponentInstance();
+
+    componentInstance.boundExchangeCache = {
+      getSync:function(){
+        return {};
+      }
+    };
+
+    expect(componentInstance.getCachedBoundExchange({
+      username:'test-user'
+    })).to.eql({});
+  });
+
+  it('tests the setCachedBoundExchange function, initialized cache', function(){
+    var ComponentInstance = require('../../../lib/system/component-instance');
+    var componentInstance = new ComponentInstance();
+
+    var cached = {};
+
+    componentInstance.boundExchangeCache = {
+      getSync:function(key){
+        return this[key];
+      },
+      setSync:function(key, value){
+        this[key] = value;
+      }
+    };
+
+    expect(componentInstance.setCachedBoundExchange('username', cached)).to.be(cached);
+    expect(componentInstance.getCachedBoundExchange('username')).to.be(cached);
+  });
+});
